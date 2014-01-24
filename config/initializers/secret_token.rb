00234a6d73580246a3168c709e0f35c5de90b583
Xachker <xaktest@bk.ru>
2014-01-24 20:43:22 +0200

    l2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opo::Application.config.secret_key_base = 'aa5789a3a75e2cd92ed22f72c6050c5d6c7aa69ec96c0b57e8f756d1dc27c6931d50c4d57faa1eb94e184e0ca0255eff38ec1c6cbe4d214e0ff74604532ae448'
